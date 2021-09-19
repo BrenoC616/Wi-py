8d7f9d69815dea713566adb166f8213a47f1cf20
Breno C <80727930+BrenoC616@users.noreply.github.com>
2021-09-19 16:58:08 -0300

    Funções em Arquivo .py Separado
    Agora as Funções ficam em 'wipyfunctions.py', fazendo com que o arquivo principal fique mais legível e que as funções fiquem mais claras em um lugar separado.
    Funções 'open_browser' e 'close_browser' foram criadas para diminuir o tamanho do arquivo principal, mas também para resolver o erro de variável 'navegador' não definida que ocorria no arquivo principal.
    Agora a Função 'validate' retorna o valor de 'device' caso a condição for verdadeira e atribui para a variável 'device' no arquivo principal, fazendo com que não ocorra o erro de variável 'device' não definida no arquivo principal.

--- wipy.py
+++ wipy.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from selenium import webdriver
-from time import sleep
-from selenium.webdriver.common.keys import Keys
 import logging
+from wipyfunctions import *
 
 logging.basicConfig(
     filename = "wipy.log", # Or /home/user/Public/YourFolder/wipy.log
@@ -12,173 +10,12 @@ logging.basicConfig(
     format = "%(asctime)s - [Nível: %(levelno)s/INFO] [Função: %(funcName)s] [Ação: %(message)s]"
 )
 
-### ELEMENTOS
-buttonAdd = 'addFilterMac'
-buttonDelete = 'deleteSelFilterMac'
-buttonLogin = 'loginBtn'
-buttonSetup = 'setupList'
-macAddress = {
-    1: "deviceMacAddress", #Ex.: af:53:3c:2f:a2:94 -> af533c2fa294
-    2: "deviceMacAddress",
-    3: "deviceMacAddress",
-    4: "deviceMacAddress",
-    5: "deviceMacAddress",
-    6: "deviceMacAddress",
-    7: "deviceMacAddress",
-    8: "deviceMacAddress",
-    9: "deviceMacAddress",
-    10: "deviceMacAddress",
-    11: "deviceMacAddress"
-}
-inputMAC = 'mac'
-optionWireless = "http://192.168.0.1/wlan_basic.htm"
-optionWirelessAdvanced = "http://192.168.0.1/wladvanced.htm"
-url = 'http://192.168.0.1/login.htm'
-
-### FUNÇÃO VALIDAÇÃO
-def validate():
-
-    global device
-    global xpath
-    global i
-
-    device = input("Diga nome do dispositivo: ")
-    
-    while True:  
-
-        if device == 'deviceName':
-            i = 1
-            xpath = '//*[@value="%s"]' % macAddress[i] #XPATH (Ex.: //*[@value="70fd46d107f4"])
-            break
-        elif device == 'deviceName':
-            i = 2
-            xpath = '//*[@value="%s"]' % macAddress[i]
-            break
-        elif device == 'deviceName':
-            i = 3
-            xpath = '//*[@value="%s"]' % macAddress[i]
-            break
-        elif device == 'deviceName':
-            i = 4
-            xpath = '//*[@value="%s"]' % macAddress[i]
-            break
-        elif device == 'deviceName':
-            i = 5
-            xpath = '//*[@value="%s"]' % macAddress[i]
-            break
-        elif device == 'deviceName':
-            i = 6
-            xpath = '//*[@value="%s"]' % macAddress[i]
-            break
-        elif device == 'deviceName':
-            i = 7
-            xpath = '//*[@value="%s"]' % macAddress[i]
-            break
-        elif device == 'deviceName':
-            i = 8
-            xpath = '//*[@value="%s"]' % macAddress[i]
-            break
-        elif device == 'deviceName':
-            i = 9
-            xpath = '//*[@value="%s"]' % macAddress[i]
-            break
-        elif device == 'deviceName':
-            i = 10
-            xpath = '//*[@value="%s"]' % macAddress[i]
-            break
-        elif device == 'deviceName':
-            i = 11
-            xpath = '//*[@value="%s"]' % macAddress[i]
-            break
-        elif device == 'exit':
-            print("Seleção de dispositivo foi fechada!")
-            break
-        else:
-            device = input("Dispositivo não existe! (Digite um dispositivo existente): ")
-
-    logging.info("Dispositivo %s escolhido!" % device)
-            
-### FUNÇÃO CONECTAR E ENTRAR
-def login_enter():
-
-    ### FUNÇÃO ENTRAR - BLOCO1
-    
-    buttonLoginElement = navegador.find_element_by_id(buttonLogin)
-
-    buttonLoginElement.click()
-
-    print("Login feito!")
-    sleep(2)
-
-    navegador.get(optionWireless)
-
-    print("Wireless acessado!")
-    sleep(2)
-
-    navegador.get(optionWirelessAdvanced)
-
-    print("Wireless Avançado acessado!")
-    sleep(2)
-    
-    ### FUNÇÃO ENTRAR BLOCO2
-    
-    buttonSetupELement = navegador.find_element_by_name(buttonSetup)
-
-    buttonSetupELement.click()
-
-    print("Cofiguração ACL acessado!")
-    sleep(2)
-
-    logging.info("Entrou na Rede!")
-
-### FUNÇÃO DELETAR
-def delete(): 
-
-    selectMACElement = navegador.find_element_by_xpath(xpath)
-
-    selectMACElement.click()
-
-    print("Dispositivo %s escolhido!" % device)
-    sleep(2)
-
-    buttonDeleteElement = navegador.find_element_by_name(buttonDelete)
-
-    buttonDeleteElement.click()
-
-    alerta = navegador.switch_to.alert
-    alerta.accept()
-
-    print("Dispositivo %s deletado da rede!" % device)
-
-    logging.info("Deletou Dispositivo %s!" % device)
-
-### FUNÇÃO ADICIONAR
-def add():   
-
-    inputMACElement = navegador.find_element_by_name(inputMAC)
-
-    inputMACElement.click()
-
-    inputMACElement.send_keys(macAddress[i])
-
-    buttonAddElement = navegador.find_element_by_name(buttonAdd)
-
-    buttonAddElement.click()
-
-    print("Dispositivo %s adicionado na rede!" % device)
-
-    logging.info("Adicionou Dispositivo %s!" % device)
-
 init = input("Quer iniciar? (s/n):")
 
 ## CONDICIONAL - INICIAR OU NÃO INICIAR
 if init == 's':
 
-    navegador = webdriver.Firefox()
-    navegador.set_window_position(16, 48)
-    navegador.set_window_size(850, 650)
-    
-    navegador.get(url)
+    open_browser()
     
     teste = input("Oque deseja realizar? (del/add):")
     
@@ -186,50 +23,45 @@ if init == 's':
     while True:
 
         if teste == 'd' or teste == 'deletar' or teste == 'del':
-            
-            validate()
+
+            device = validate()
             
             ## CONDICIONAL - CONTINUAR PROCESSO DE DELETAR OU ENCERRAR
             if device == 'exit':
 
                 print("Programa encerrado!")
-                sleep(2)
-                navegador.quit()
+                close_browser(2)
                 break
                 
             else:
 
                 login_enter()
                 delete()
-                sleep(2)
-                navegador.quit()
+                close_browser(2)
                 break
                 
         elif teste == 'a' or teste == 'adicionar' or teste == 'add':
             
-            validate()
+            device = validate()
             
             ## CONDICIONAL - CONTINUAR PROCESSO DE ADICIONAR OU ENCERRAR
             if device == 'exit':
 
                 print("Programa encerrado!")
-                sleep(2)
-                navegador.quit()
+                close_browser(2)
                 break
                 
             else:
 
                 login_enter()
                 add()
-                sleep(2)
-                navegador.quit()
+                close_browser(2)
                 break
                 
         elif teste == 'exit':
 
             print("Programa encerrado!")
-            sleep(2)
-            navegador.quit()
+            close_browser(2)
             break
         
         ## CONDIÇÃO CASO NÃO SEJA ADICIONAR, DELETAR OU ENCERRAR
---
+++ wipyfunctions.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.keys import Keys
+import logging
+
+### ELEMENTOS
+buttonAdd = 'addFilterMac'
+buttonDelete = 'deleteSelFilterMac'
+buttonLogin = 'loginBtn'
+buttonSetup = 'setupList'
+macAddress = {
+    1: "deviceMacAddress", #Ex.: af:53:3c:2f:a2:94 -> af533c2fa294
+    2: "deviceMacAddress",
+    3: "deviceMacAddress",
+    4: "deviceMacAddress",
+    5: "deviceMacAddress",
+    6: "deviceMacAddress",
+    7: "deviceMacAddress",
+    8: "deviceMacAddress",
+    9: "deviceMacAddress",
+    10: "deviceMacAddress",
+    11: "deviceMacAddress"
+}
+inputMAC = 'mac'
+optionWireless = "http://192.168.0.1/wlan_basic.htm"
+optionWirelessAdvanced = "http://192.168.0.1/wladvanced.htm"
+url = 'http://192.168.0.1/login.htm'
+
+### FUNÇÃO: ABRIR NAGEVADOR E ACESSAR URL
+def open_browser():
+
+    global navegador
+
+    navegador = webdriver.Firefox()
+
+    navegador.set_window_position(16, 48)
+    navegador.set_window_size(850, 650)
+    
+    navegador.get(url)
+
+### FUNÇÃO: VALIDAÇÃO
+def validate():
+
+    global device
+    global xpath
+    global i
+
+    device = input("Diga nome do dispositivo: ")
+    
+    while True:  
+        
+        if device == 'a10Joao':
+            i = 1
+            xpath = '//*[@value="%s"]' % macAddress[i] #XPATH (Ex.: //*[@value="af533c2fa294"])
+            return device
+            break
+        elif device == 'a10Breno':
+            i = 2
+            xpath = '//*[@value="%s"]' % macAddress[i]
+            return device
+            break
+        elif device == 'tvQuartoBreno':
+            i = 3
+            xpath = '//*[@value="%s"]' % macAddress[i]
+            return device
+            break
+        elif device == 'tvSala':
+            i = 4
+            xpath = '//*[@value="%s"]' % macAddress[i]
+            return device
+            break
+        elif device == 'tvQuartoBruna':
+            i = 5
+            xpath = '//*[@value="%s"]' % macAddress[i]
+            return device
+            break
+        elif device == 'tvBox':
+            i = 6
+            xpath = '//*[@value="%s"]' % macAddress[i]
+            return device
+            break
+        elif device == 'e6Mae':
+            i = 7
+            xpath = '//*[@value="%s"]' % macAddress[i]
+            return device
+            break
+        elif device == 'noteBreno':
+            i = 8
+            xpath = '//*[@value="%s"]' % macAddress[i]
+            return device
+            break
+        elif device == 'noteBruna':
+            i = 9
+            xpath = '//*[@value="%s"]' % macAddress[i]
+            return device
+            break
+        elif device == 'a32Bruna':
+            i = 10
+            xpath = '//*[@value="%s"]' % macAddress[i]
+            return device
+            break
+        elif device == 'hpPrint':
+            i = 11
+            xpath = '//*[@value="%s"]' % macAddress[i]
+            return device
+            break
+        elif device == 'exit':
+            print("Seleção de dispositivo foi fechada!")
+            return device
+            break
+        else:
+            device = input("Dispositivo não existe! (Digite um dispositivo existente): ")
+
+    logging.info("Dispositivo %s escolhido!" % device)
+            
+### FUNÇÃO: CONECTAR NO ROTEADOR E ENTRAR EM CONFIG ACL
+def login_enter():
+
+    ### FUNÇÃO ENTRAR - BLOCO1
+    
+    buttonLoginElement = navegador.find_element_by_id(buttonLogin)
+
+    buttonLoginElement.click()
+
+    print("Login feito!")
+    sleep(2)
+
+    navegador.get(optionWireless)
+
+    print("Wireless acessado!")
+    sleep(2)
+
+    navegador.get(optionWirelessAdvanced)
+
+    print("Wireless Avançado acessado!")
+    sleep(2)
+    
+    ### FUNÇÃO ENTRAR BLOCO2
+    
+    buttonSetupELement = navegador.find_element_by_name(buttonSetup)
+
+    buttonSetupELement.click()
+
+    print("Cofiguração ACL acessado!")
+    sleep(2)
+
+    logging.info("Entrou na Rede!")
+
+### FUNÇÃO: DELETAR
+def delete(): 
+
+    selectMACElement = navegador.find_element_by_xpath(xpath)
+
+    selectMACElement.click()
+
+    print("Dispositivo %s escolhido!" % device)
+    sleep(2)
+
+    buttonDeleteElement = navegador.find_element_by_name(buttonDelete)
+
+    buttonDeleteElement.click()
+
+    alerta = navegador.switch_to.alert
+    alerta.accept()
+
+    print("Dispositivo %s deletado da rede!" % device)
+
+    logging.info("Deletou Dispositivo %s!" % device)
+
+### FUNÇÃO: ADICIONAR
+def add():   
+
+    inputMACElement = navegador.find_element_by_name(inputMAC)
+
+    inputMACElement.click()
+
+    inputMACElement.send_keys(macAddress[i])
+
+    buttonAddElement = navegador.find_element_by_name(buttonAdd)
+
+    buttonAddElement.click()
+
+    print("Dispositivo %s adicionado na rede!" % device)
+
+    logging.info("Adicionou Dispositivo %s!" % device)
+
+### FUNÇÃO: FECHAR NAVEGADOR
+def close_browser(secs):
+
+    sleep(secs)
+
+    navegador.quit()
