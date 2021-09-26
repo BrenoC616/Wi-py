4a2ba8b5c597cf893f63bf448ebf7f38e72187fc
Breno C <breno.cesa99.97@gmail.com>
2021-09-25 21:27:19 -0300

    Atualizando importação de funções e '.gitignore'

--- wipy.py
+++ wipy.py
@@ -2,20 +2,20 @@
 # coding: utf-8
 
 import logging
-from wipyfunctions import *
+import wipyfunctions
 
 logging.basicConfig(
     filename = "wipy.log", # Or /home/Your-User/Public/YourFolder/wipy.log
     level = logging.INFO, 
     format = "%(asctime)s - [Nível: %(levelno)s/INFO] [Função: %(funcName)s] [Ação: %(message)s]"
-)
+)   
 
 init = input("Quer iniciar? (s/n):")
 
 ## CONDICIONAL - INICIAR OU NÃO INICIAR
 if init == 's':
 
-    open_browser()
+    wipyfunctions.open_browser()
     
     teste = input("Oque deseja realizar? (del/add):")
     
@@ -24,44 +24,44 @@ if init == 's':
 
         if teste == 'd' or teste == 'deletar' or teste == 'del':
 
-            device = validate()
+            device = wipyfunctions.validate()
             
             ## CONDICIONAL - CONTINUAR PROCESSO DE DELETAR OU ENCERRAR
             if device == 'exit':
 
                 print("Programa encerrado!")
-                close_browser(2)
+                wipyfunctions.close_browser(2)
                 break
                 
             else:
 
-                login_enter()
-                delete()
-                close_browser(2)
+                wipyfunctions.login_enter()
+                wipyfunctions.delete()
+                wipyfunctions.close_browser(2)
                 break
                 
         elif teste == 'a' or teste == 'adicionar' or teste == 'add':
             
-            device = validate()
+            device = wipyfunctions.validate()
             
             ## CONDICIONAL - CONTINUAR PROCESSO DE ADICIONAR OU ENCERRAR
             if device == 'exit':
 
                 print("Programa encerrado!")
-                close_browser(2)
+                wipyfunctions.close_browser(2)
                 break
                 
             else:
 
-                login_enter()
-                add()
-                close_browser(2)
+                wipyfunctions.login_enter()
+                wipyfunctions.add()
+                wipyfunctions.close_browser(2)
                 break
                 
         elif teste == 'exit':
 
             print("Programa encerrado!")
-            close_browser(2)
+            wipyfunctions.close_browser(2)
             break
         
         ## CONDIÇÃO CASO NÃO SEJA ADICIONAR, DELETAR OU ENCERRAR
