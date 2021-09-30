2c5e71c350e8ed03919b985f9165cb2a043d50f1
Breno C <breno.cesa99.97@gmail.com>
2021-09-30 02:55:13 -0300

    Formatando codigos

--- source/functions/wipyfunctions.py
+++ source/functions/wipyfunctions.py
@@ -8,158 +8,131 @@ import logging
 import json
 import configparser
 
-config = configparser.ConfigParser()
-
-### ELEMENTOS
-with open("source/json/elements.json", "r") as elementsData: 
+with open("source/json/elements.json", "r") as elementsData:
     elements = json.load(elementsData)
 
-### ENDEREÇOS MAC
-with open("source/json/wirelessClients.json", "r") as wirelessClientsData:  
+with open("source/json/wirelessClients.json", "r") as wirelessClientsData:
     wirelessClients = json.load(wirelessClientsData)
 
-### FUNÇÃO: DEFINIR NAVEGADOR
-def set_browser():
+config = configparser.ConfigParser()
 
+def set_browser():
     filepath = "source/config/config.ini"
 
     config.read(filepath)
-
     configDefault = config["default"]
-
     configDefaultSet = configDefault["set"]
 
     if configDefaultSet == "0":
         value = input("Defina seu navegador (Chrome/Firefox): ")
-
         configDefault["browser"] = value
         configDefault["set"] = "1"
 
         with open(filepath, "w") as configfile:
             config.write(configfile)
 
-### FUNÇÃO: ABRIR NAGEVADOR E ACESSAR URL
+
 def open_browser():
+    global browser
 
     filepath = "source/config/config.ini"
 
     config.read(filepath)
-
     configDefault = config["default"]
-
     configDefaultBrowser = configDefault["browser"]
-
-    global browser
 
     if configDefaultBrowser == "Firefox":
         browser = webdriver.Firefox()
-            
+
     elif configDefaultBrowser == "Chrome":
         chromeOptions = webdriver.ChromeOptions()
-        chromeOptions.add_argument("--remote-debugging-port=9222") 
+        chromeOptions.add_argument("--remote-debugging-port=9222")
         browser = webdriver.Chrome(chrome_options=chromeOptions)
 
     browser.set_window_position(16, 48)
     browser.set_window_size(850, 650)
-    
     browser.get(elements["URL"])
 
-### FUNÇÃO: VALIDAÇÃO
-def validate():
 
+def validate():
     global device
     global xpath
 
     device = input("Diga nome do dispositivo: ")
-    
-    while True:  
-        
+
+    while True:
         if device in wirelessClients:
-            xpath = '//*[@value="%s"]' % wirelessClients[f"{device}"]["reduced-mac-address"] #XPATH (Ex.: //*[@value="71f34vd707fk"])
+            xpath = '//*[@value="%s"]' % wirelessClients[f"{device}"]["reduced-mac-address"]
             break
+
         elif device == 'exit':
             print("Seleção de dispositivo foi fechada!")
             return device
             break
+        
         else:
-            device = input("Dispositivo não existe! (Digite um dispositivo existente): ")
+            device = input(
+                "Dispositivo não existe! (Digite um dispositivo existente): ")
 
     logging.info(f"Dispositivo {device} escolhido!")
-            
-### FUNÇÃO: CONECTAR NO ROTEADOR E ENTRAR EM CONFIG ACL
+
+
 def login_enter():
-
-    ### FUNÇÃO ENTRAR - BLOCO1
-    
-    buttonLoginElement = browser.find_element_by_id(elements["Buttons"]["buttonLogin"])
-
+    buttonLoginElement = browser.find_element_by_id(
+        elements["Buttons"]["buttonLogin"])
     buttonLoginElement.click()
-
     print("Login feito!")
     sleep(2)
 
     browser.get(elements["Options"]["optionWireless"])
-
     print("Wireless acessado!")
     sleep(2)
 
     browser.get(elements["Options"]["optionWirelessAdvanced"])
-
     print("Wireless Avançado acessado!")
     sleep(2)
-    
-    ### FUNÇÃO ENTRAR BLOCO2
-    
-    buttonSetupELement = browser.find_element_by_name(elements["Buttons"]["buttonSetup"])
 
+    buttonSetupELement = browser.find_element_by_name(
+        elements["Buttons"]["buttonSetup"])
     buttonSetupELement.click()
-
     print("Cofiguração ACL acessado!")
     sleep(2)
 
     logging.info("Entrou na Rede!")
 
-### FUNÇÃO: DELETAR
-def delete(): 
 
+def delete():
     selectMACElement = browser.find_element_by_xpath(xpath)
-
     selectMACElement.click()
-
     print(f"Dispositivo {device} escolhido!")
     sleep(2)
 
-    buttonDeleteElement = browser.find_element_by_name(elements["Buttons"]["buttonDelete"])
-
+    buttonDeleteElement = browser.find_element_by_name(
+        elements["Buttons"]["buttonDelete"])
     buttonDeleteElement.click()
 
-    alerta = browser.switch_to.alert
-    alerta.accept()
-
+    alert = browser.switch_to.alert
+    alert.accept()
     print(f"Dispositivo {device} deletado da rede!")
 
     logging.info(f"Deletou Dispositivo {device}!")
 
-### FUNÇÃO: ADICIONAR
-def add():   
 
-    inputMACElement = browser.find_element_by_name(elements["Inputs"]["inputMAC"])
-
+def add():
+    inputMACElement = browser.find_element_by_name(
+        elements["Inputs"]["inputMAC"])
     inputMACElement.click()
+    inputMACElement.send_keys(
+        wirelessClients[f"{device}"]["reduced-mac-address"])
 
-    inputMACElement.send_keys(wirelessClients[f"{device}"]["reduced-mac-address"])
-
-    buttonAddElement = browser.find_element_by_name(elements["Buttons"]["buttonAdd"])
-
+    buttonAddElement = browser.find_element_by_name(
+        elements["Buttons"]["buttonAdd"])
     buttonAddElement.click()
-
     print(f"Dispositivo {device} adicionado na rede!")
 
     logging.info(f"Adicionou Dispositivo {device}!")
 
-### FUNÇÃO: FECHAR NAVEGADOR
+
 def close_browser(secs=2):
-
     sleep(secs)
-
     browser.quit()
--- wipy.py
+++ wipy.py
@@ -5,71 +5,53 @@ import logging
 from source.functions import wipyfunctions
 
 logging.basicConfig(
-    filename = "log/wipy.log", # Or /home/Your-User/Public/YourFolder/Wi-py/log/wipy.log
-    level = logging.INFO, 
-    format = "%(asctime)s - [Nível: %(levelno)s/INFO] [Função: %(funcName)s] [Ação: %(message)s]"
-)   
+    filename="log/wipy.log",
+    level=logging.INFO,
+    format="%(asctime)s - [Nível: %(levelno)s/INFO] [Função: %(funcName)s] [Ação: %(message)s]")
 
 init = input("Quer iniciar? (s/n):")
-
 wipyfunctions.set_browser()
 
-## CONDICIONAL - INICIAR OU NÃO INICIAR
 if init == 's':
-
     wipyfunctions.open_browser()
-    
     action = input("Oque deseja realizar? (del/add):")
     
-    ## LOOP CONDICIONAL - DELETAR MAC, ADICIONAR MAC OU ENCERRAR
     while True:
-
         if action == 'd' or action == 'deletar' or action == 'del':
-
             device = wipyfunctions.validate()
             
-            ## CONDICIONAL - CONTINUAR PROCESSO DE DELETAR OU ENCERRAR
-            if device == 'exit':
-
-                print("Programa encerrado!")
-                wipyfunctions.close_browser()
-                break
-                
-            else:
-
+            if device != 'exit':
                 wipyfunctions.login_enter()
                 wipyfunctions.delete()
                 wipyfunctions.close_browser()
                 break
                 
-        elif action == 'a' or action == 'adicionar' or action == 'add':
-            
-            device = wipyfunctions.validate()
-            
-            ## CONDICIONAL - CONTINUAR PROCESSO DE ADICIONAR OU ENCERRAR
-            if device == 'exit':
-
+            else:
                 print("Programa encerrado!")
                 wipyfunctions.close_browser()
                 break
-                
-            else:
-
+                              
+        elif action == 'a' or action == 'adicionar' or action == 'add':
+            device = wipyfunctions.validate()
+            
+            if device != 'exit':
                 wipyfunctions.login_enter()
                 wipyfunctions.add()
                 wipyfunctions.close_browser()
                 break
                 
+            else:
+                print("Programa encerrado!")
+                wipyfunctions.close_browser()
+                break
+                
         elif action == 'exit':
-
             print("Programa encerrado!")
             wipyfunctions.close_browser()
             break
-        
-        ## CONDIÇÃO CASO NÃO SEJA ADICIONAR, DELETAR OU ENCERRAR
+
         else:
-
             action = input("Opção inválida! (digite deletar/del/d ou adicionar/add/a):")
+            
 else:
-
     print("Não iniciado!")
